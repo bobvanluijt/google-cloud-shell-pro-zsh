2ce6ead346d05d4596b5518e9c3721fd92f94a2a
Bob van Luijt <bob@kubrickolo.gy>
2017-03-11 07:34:06 -0600

    Update for auto reinstall

--- install.sh
+++ install.sh
@@ -94,6 +94,11 @@ main() {
     # If this platform provides a "chsh" command (not Cygwin), do it, man!
     if hash chsh >/dev/null 2>&1; then
       printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+      echo 'if ! dpkg-query -l zsh > /dev/null; then'
+      echo '    sudo rm -r ~/.oh-my-zsh'
+      echo '    sudo rm .zsh*'
+      echo '    sh -c "$(curl -fsSL https://raw.githubusercontent.com/bobvanluijt/google-cloud-shell-pro-zsh/master/install.sh)"'
+      echo 'fi'
       echo '$(grep /zsh$ /etc/shells | tail -1)' >> ~/.bashrc
     # Else, suggest the user do so manually.
     else
@@ -107,7 +112,7 @@ main() {
   ##
   cd ~/.oh-my-zsh/plugins
   git clone https://github.com/littleq0903/gcloud-zsh-completion.git
-  echo "fpath=(~/.oh-my-zsh/plugins/gcloud-zsh-completion/src ~/.oh-my-zsh/plugins/git ~/.oh-my-zsh/functions ~/.oh-my-zsh/completions /usr/local/share/zsh/site-functions /usr/share/zsh/vendor-functions /usr/share/zsh/vendor-completions /usr/share/zsh/functions/Calendar /usr/share/zsh/functions/Chpwd /usr/share/zsh/functions/Completion /usr/share/zsh/functions/Completion/AIX /usr/share/zsh/functions/Completion/BSD /usr/share/zsh/functions/Completion/Base /usr/share/zsh/functions/Completion/Cygwin /usr/share/zsh/functions/Completion/Darwin /usr/share/zsh/functions/Completion/Debian /usr/share/zsh/functions/Completion/Linux /usr/share/zsh/functions/Completion/Mandriva /usr/share/zsh/functions/Completion/Redhat /usr/share/zsh/functions/Completion/Solaris /usr/share/zsh/functions/Completion/Unix /usr/share/zsh/functions/Completion/X /usr/share/zsh/functions/Completion/Zsh /usr/share/zsh/functions/Completion/openSUSE /usr/share/zsh/functions/Exceptions /usr/share/zsh/functions/MIME /usr/share/zsh/functions/Misc /usr/share/zsh/functions/Newuser /usr/share/zsh/functions/Prompts /usr/share/zsh/functions/TCP /usr/share/zsh/functions/VCS_Info /usr/share/zsh/functions/VCS_Info/Backends /usr/share/zsh/functions/Zftp /usr/share/zsh/functions/Zle)" >> ~/.zshrc
+  echo "fpath=(~/.oh-my-zsh/plugins/gcloud-zsh-completion/docker-compose ~/.oh-my-zsh/plugins/docker ~/.oh-my-zsh/plugins/git ~/.oh-my-zsh/functions ~/.oh-my-zsh/completions /usr/local/share/zsh/site-functions /usr/share/zsh/vendor-functions /usr/share/zsh/vendor-completions /usr/share/zsh/functions/Calendar /usr/share/zsh/functions/Chpwd /usr/share/zsh/functions/Completion /usr/share/zsh/functions/Completion/AIX /usr/share/zsh/functions/Completion/BSD /usr/share/zsh/functions/Completion/Base /usr/share/zsh/functions/Completion/Cygwin /usr/share/zsh/functions/Completion/Darwin /usr/share/zsh/functions/Completion/Debian /usr/share/zsh/functions/Completion/Linux /usr/share/zsh/functions/Completion/Mandriva /usr/share/zsh/functions/Completion/Redhat /usr/share/zsh/functions/Completion/Solaris /usr/share/zsh/functions/Completion/Unix /usr/share/zsh/functions/Completion/X /usr/share/zsh/functions/Completion/Zsh /usr/share/zsh/functions/Completion/openSUSE /usr/share/zsh/functions/Exceptions /usr/share/zsh/functions/MIME /usr/share/zsh/functions/Misc /usr/share/zsh/functions/Newuser /usr/share/zsh/functions/Prompts /usr/share/zsh/functions/TCP /usr/share/zsh/functions/VCS_Info /usr/share/zsh/functions/VCS_Info/Backends /usr/share/zsh/functions/Zftp /usr/share/zsh/functions/Zle)" >> ~/.zshrc
   echo "autoload -U compinit compdef" >> ~/.zshrc
   echo "compinit" >> ~/.zshrc
   echo "cd ~" >> ~/.zshrc
